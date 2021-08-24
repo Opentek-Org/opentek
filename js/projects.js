a31642dba9a790db44ae229f0add6b5583d89ee2
Nishikantaray <nishikantaray1@gmail.com>
2021-08-24 17:26:11 +0530

    Added Projects page

---
+++ js/projects.js
@@ -0,0 +1,87 @@
+let domain = [
+    "webdev",
+    "ml",
+    "frontend"
+  ];
+ 
+  let htmlc = `<select class="custom-select mr-sm-2 " id="type">`;
+  htmlc += `<option>Choose your Domain</option>`;
+  for (let index = 0; index < domain.length; index++) {
+    htmlc += `<option value="${index}">${domain[index]}</option>`;
+  }
+  
+  htmlc += "</select>";
+  $("#selectoption").append(htmlc);
+  
+  let lang = [
+    "HTML",
+    "CSS",
+    "NODEJS"
+  ];
+  let htmld = `<select class="custom-select mr-sm-2 " id="type">`;
+  htmld += `<option>Choose your Language</option>`;
+  for (let index = 0; index < lang.length; index++) {
+    htmld += `<option value="${index}">${lang[index]}</option>`;
+  }
+  
+  htmld += "</select>";
+  $("#selectOptionTwo").append(htmld);
+  
+  let data = [
+   
+  ];
+  
+  let valu;
+  $(function () {
+    $("#type").change(function () {
+      valu = $(this).val();
+      // console.log(valu);
+      let index = valu;
+      $("#program").empty();
+      $("#error").empty();
+      if (valu < data.length && data[index].Data.length > 0) {
+        for (let i = 0; i < data[index].Data.length; i++) {
+          try {
+            // console.log(data);
+            var html = '<div class="row">';
+            for (i = 0; i < data[index].Data.length; i++) {
+              if (i % 3 == 0 && i != 0) {
+                html += "</div>";
+                html += '<div class="row">';
+              }
+  
+              html += `<div class="col-lg-9 col-12 m-auto">
+                      
+                              <div class="over-item txt-right unique-style3 rounded-3">
+                                    <div class="row">
+                                        <div class="col-md-8 col-12 ">
+                                        <div class="break"></div>
+                                        <h3 class="text-left" >${data[index].Data[i].details.eventName}</h3>
+                                        <h6 class="text-left text-light" >${data[index].Data[i].details.eventDate}</h6></br>
+                                        <p class="text-left">${data[index].Data[i].details.eventDesc}</p>
+                                        <div class="over-btn text-left">
+                                            <a href ="${data[index].Data[i].details.eventurl}" target="blank">Website</a>
+                                        </div>
+                                        </div>
+                                        <div class="col-md-4 col-12">
+                                        <img src="${data[index].Data[i].details.eventImg}" height="200px" alt="" srcset=""></div>
+                                    </div>
+                                  
+                                  
+                              </div></div>`;
+            }
+  
+            $("#program").append(html);
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      } else {
+        var htmlz = `<div class= "container unique-style3  mb-5 pb-5">`;
+        htmlz += `<p class="text-light text-center text-no-data">No data found please select another month</p>`;
+        htmlz += `</div>`;
+        $("#error").append(htmlz);
+      }
+    });
+  });
+  
