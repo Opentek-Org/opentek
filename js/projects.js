0010f887bdacf1215f7b230d30db3caafc0d173d
Anirudh Panda <anirudhpandaofficial@gmail.com>
2021-08-25 16:24:39 +0530

    projects page update

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,4 @@
-let domain = ["WEBDEV", "ML", "RESTAPI","DSA","NODEJS"];
+let domain = ["Web Development", "Machine Learning", "API", "DSA", "NODEJS"];
 
 let htmlc = `<select class="custom-select mr-sm-2 " id="type">`;
 htmlc += `<option value="Choose your Domain" disabled selected>Choose your Domain</option>`;
@@ -9,114 +9,165 @@ for (let index = 0; index < domain.length; index++) {
 htmlc += "</select>";
 $("#selectoption").append(htmlc);
 
-
 let data = [
-    {
-        domainId:"0",
-        domainName:"WEBDEV",
-        Data:[
-            {
-                langId:"0",
-                langTitle:["HTML","CSS","JS"],
-                langImg:"https://picsum.photos/500/300/?image=5",
-                langName:"projectname",
-                langDesc:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-                langLevel:"basic",
-                langurl:"#",
-                langAdmin:"Nishi"
-            },
-            {
-                langId:"0",
-                langTitle:["HTML","CSS","JS"],
-                langImg:"https://picsum.photos/500/300/?image=5",
-                langName:"projectname",
-                langDesc:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-                langLevel:"basic",
-                langurl:"#",
-                langAdmin:"Nishi"
-            },
-            {
-                langId:"0",
-                langTitle:["HTML","CSS","JS"],
-                langImg:"https://picsum.photos/500/300/?image=5",
-                langName:"projectname",
-                langDesc:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-                langLevel:"basic",
-                langurl:"#",
-                langAdmin:"Nishi"
-            },
-        ]
-    },
-    {
-        domainId:"1",
-        Data:[
-            {
-                langId:"0",
-                langTitle:["HTML","CSS","JS"],
-                langImg:"https://picsum.photos/500/300/?image=5",
-                langName:"projectname",
-                langDesc:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-                langLevel:"basic",
-                langurl:"#",
-                langAdmin:"Nishi"
-            },
-            {
-                langId:"0",
-                langTitle:["HTML","CSS","JS"],
-                langImg:"https://picsum.photos/500/300/?image=5",
-                langName:"projectname",
-                langDesc:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-                langLevel:"basic",
-                langurl:"#",
-                langAdmin:"Nishi"
-            },
-            {
-                langId:"0",
-                langTitle:["HTML","CSS","JS"],
-                langImg:"https://picsum.photos/500/300/?image=5",
-                langName:"projectname",
-                langDesc:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-                langLevel:"basic",
-                langurl:"#",
-                langAdmin:"Nishi"
-            },
-        ]
-    },
-    {
-        domainId:"2",
-        Data:[
-            {
-                langId:"0",
-                langTitle:["HTML","CSS","JS"],
-                langImg:"https://picsum.photos/500/300/?image=5",
-                langName:"projectname",
-                langDesc:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-                langLevel:"basic",
-                langurl:"#",
-                langAdmin:"Nishi"
-            },
-            {
-                langId:"0",
-                langTitle:["HTML","CSS","JS"],
-                langImg:"https://picsum.photos/500/300/?image=5",
-                langName:"projectname",
-                langDesc:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-                langLevel:"basic",
-                langurl:"#",
-                langAdmin:"Nishi"
-            },
-            {
-                langId:"0",
-                langTitle:["HTML","CSS","JS"],
-                langImg:"https://picsum.photos/500/300/?image=5",
-                langName:"projectname",
-                langDesc:"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-                langLevel:"basic",
-                langurl:"#",
-                langAdmin:"Nishi"
-            },
-        ]
-    }
+  {
+    domainId: "0",
+    // domainName: "Web Development",
+    Data: [
+      {
+        langId: "0",
+        langTitle: ["HTML", "SCSS", "JS"],
+        // langImg: "../images/projects/doc2pen.gif",
+        langName: "Doc2Pen",
+        langDesc:
+          "Doc2Pen is the 1 stop shop for getting all your handmade assignments ready for submission digitally. <br/>Check it out and contribute!",
+        // langLevel: "basic",
+        langurl: "https://github.com/smaranjitghose/doc2pen",
+        langAdmin: "Smaranjit Ghose",
+      },
+      {
+        langId: "1",
+        langTitle: ["HTML", "CSS", "JS"],
+        // langImg: "https://picsum.photos/500/300/?image=5",
+        langName: "Awesome Portfolio Website",
+        langDesc:
+          "A community maintained open source project aimed at making personal portfolio for researchers, developers and analysts simple, fast and less cumbersome.",
+        // langLevel: "basic",
+        langurl: "https://github.com/smaranjitghose/awesome-portfolio-websites",
+        langAdmin: "Smaranjit Ghose",
+      },
+      {
+        langId: "2",
+        langTitle: ["JavaScript"],
+        // langImg: "https://picsum.photos/500/300/?image=5",
+        langName: "Manthan",
+        langDesc:
+          "We provide an online testing platform that can be used to conduct tests that are wrapped with all the necessary precautions to reduce the chances of cheating.",
+        // langLevel: "basic",
+        langurl: "https://github.com/Manthan933/Manthan",
+        langAdmin: "Manthan",
+      },
+    ],
+  },
+  {
+    domainId: "1",
+    // domainName: "ML",
+    Data: [
+      {
+        langId: "0",
+        langTitle: ["Python"],
+        // langImg: "../images/projects/jina.gif",
+        langName: "Jina",
+        langDesc: "Cloud-native neural search framework for 𝙖𝙣𝙮 kind of data",
+        // langLevel: "basic",
+        langurl: "https://github.com/jina-ai/jina",
+        langAdmin: "Jina AI",
+      },
+      {
+        langId: "1",
+        langTitle: ["Jupyter Notebook"],
+        // langImg: "https://picsum.photos/500/300/?image=5",
+        langName: "FACE-X",
+        langDesc:
+          "Demonstration of different algorithms and operations on faces.",
+        // langLevel: "basic",
+        langurl: "https://github.com/akshitagupta15june/Face-X",
+        langAdmin: "AKSHITA GUPTA",
+      },
+      {
+        langId: "2",
+        langTitle: ["Python", "Flask", "Numpy"],
+        // langImg: "https://picsum.photos/500/300/?image=5",
+        langName: "Breast-Cancer-Predictor",
+        langDesc: "A Flask based web application to predict breast cancer.",
+        // langLevel: "basic",
+        langurl:
+          "https://github.com/srimani-programmer/Breast-Cancer-Predictor",
+        langAdmin: "Srimani",
+      },
+    ],
+  },
+  {
+    domainId: "2",
+    // domainName: "API",
+    Data: [
+      {
+        langId: "0",
+        langTitle: ["DS", "Algorithms"],
+        // langImg: "../images/projects/jina.gif",
+        langName: "NeoAlgo",
+        langDesc:
+          "Bringing all Data Structures and Algorithms under one Roof ⚡",
+        // langLevel: "basic",
+        langurl: "https://github.com/TesseractCoding/NeoAlgo",
+        langAdmin: "Tesseract Coding",
+      },
+      {
+        langId: "1",
+        langTitle: ["Jupyter Notebook"],
+        // langImg: "https://picsum.photos/500/300/?image=5",
+        langName: "FACE-X",
+        langDesc:
+          "Demonstration of different algorithms and operations on faces.",
+        // langLevel: "basic",
+        langurl: "https://github.com/akshitagupta15june/Face-X",
+        langAdmin: "AKSHITA GUPTA",
+      },
+      {
+        langId: "2",
+        langTitle: ["Python", "Flask", "Numpy"],
+        // langImg: "https://picsum.photos/500/300/?image=5",
+        langName: "Breast-Cancer-Predictor",
+        langDesc: "A Flask based web application to predict breast cancer.",
+        // langLevel: "basic",
+        langurl:
+          "https://github.com/srimani-programmer/Breast-Cancer-Predictor",
+        langAdmin: "Srimani",
+      },
+    ],
+  },
+//   DSA BELOW id 3
+  {
+    domainId: "3",
+    Data: [
+      {
+        langId: "0",
+        langTitle: ["HTML", "SCSS", "JS"],
+        // langImg: "../images/projects/doc2pen.gif",
+        langName: "DSAAAAA",
+        langDesc:
+          "Doc2Pen is the 1 stop shop for getting all your handmade assignments ready for submission digitally. <br/>Check it out and contribute!",
+        // langLevel: "basic",
+        langurl: "https://github.com/smaranjitghose/doc2pen",
+        langAdmin: "Smaranjit Ghose",
+      },
+      {
+        langId: "1",
+        langTitle: ["HTML", "CSS", "JS"],
+        // langImg: "https://picsum.photos/500/300/?image=5",
+        langName: "Awesome Portfolio Website",
+        langDesc:
+          "A community maintained open source project aimed at making personal portfolio for researchers, developers and analysts simple, fast and less cumbersome.",
+        // langLevel: "basic",
+        langurl: "https://github.com/smaranjitghose/awesome-portfolio-websites",
+        langAdmin: "Smaranjit Ghose",
+      },
+      {
+        langId: "2",
+        langTitle: ["JavaScript"],
+        // langImg: "https://picsum.photos/500/300/?image=5",
+        langName: "Manthan",
+        langDesc:
+          "We provide an online testing platform that can be used to conduct tests that are wrapped with all the necessary precautions to reduce the chances of cheating.",
+        // langLevel: "basic",
+        langurl: "https://github.com/Manthan933/Manthan",
+        langAdmin: "Manthan",
+      },
+    ],
+  },
+  {
+
+  }
 ];
 
 let valu;
@@ -125,32 +176,30 @@ $(function () {
     valu = $(this).val();
     console.log(valu);
     let index = valu;
-    $("#program").empty();
+    $("#project").empty();
     $("#error").empty();
-    if (valu < data.length && valu<data[index].Data.length) {
+    if (valu < data.length && valu < data[index].Data.length) {
       for (let i = 0; i < data[index].Data.length; i++) {
         try {
           // console.log(data);
-          var html = '';
+          var html = "";
           for (i = 0; i < data[index].Data.length; i++) {
-
             html += `
                 <li class="cards_item">
                 <div class="card">
-                    <div class="card_image"><img src="${data[index].Data[i].langImg}"></div>
                     <div class="card_content">
-                    <p class="card_title">${data[index].Data[i].langName}<span class="card_title text-light text-right">(${data[index].Data[i].langLevel})</span></p>
-                    <span class="cardtitle">ProjectAdmin-${data[index].Data[i].langAdmin}</span>
-                    <h6 class="card_title">${data[index].Data[i].langTitle}</h6>
+                    <p class="card_title">${data[index].Data[i].langName}</p>
+                    <span class="cardtitle">Project Admin-${data[index].Data[i].langAdmin}</span>
+                    <h6 class="card_title">Tech Stack- ${data[index].Data[i].langTitle}</h6>
                     <p class="card_text">${data[index].Data[i].langDesc}</p>
-                    <button class="btn card_btn" onclick="window.location.href='${data[index].Data[i].langurl}'">Repo Url</button>
+                    <button class="btn card_btn" onclick="window.location.href='${data[index].Data[i].langurl}'"target="blank">Repo Url</button>
                     </div>
                 </div>
                 </li>
         
                 `;
           }
-          $("#program").append(html);
+          $("#project").append(html);
         } catch (error) {
           console.log(error);
         }
