d96b4c5801bd03461b18d6703b66a86d1cff4bfd
neeraj15022001 <gneeraj32595@gmai.com>
2021-10-01 03:48:42 +0530

    added new card design

--- js/projects.js
+++ js/projects.js
@@ -27,16 +27,24 @@ $(function () {
           var html = "";
           for (i = 0; i < data[index].Data.length; i++) {
             html += `
-                <li class="cards_item">
-                <div class="card">
-                    <div class="card_content">
-                    <p class="card_title">${data[index].Data[i].langName}</p>
-                    <span class="cardtitle">Project Admin-${data[index].Data[i].langAdmin}</span>
-                    <h6 class="card_title">Tech Stack- ${data[index].Data[i].langTitle}</h6>
-                    <p class="card_text">${data[index].Data[i].langDesc}</p>
-                    <button class="btn card_btn" onclick="window.location.href='${data[index].Data[i].langurl}'"target="blank">Repo Url</button>
+            <li class="custom-card-item col-12 col-lg-4 m-auto">
+                    <div class="custom-card text-white d-flex flex-column justify-content-between position-relative">
+                        <div class="mb-3">
+                            <p class="custom-card-title text-capitalize fs-3 mb-2">${data[index].Data[i].langName}</p>
+                            <p class="custom-card-description">${data[index].Data[i].langDesc}</p>
+                        </div>
+                        <div class="mb-3">
+                            <p class="headline text-capitalize">project admin :</p>
+                            <p class="subtitle text-capitalize">${data[index].Data[i].langAdmin}</p>
+                        </div>
+                        <div>
+                            <p class="headline text-capitalize">tech stack :</p>
+                            <ul>
+                                <li class="text-uppercase lh-base">${data[index].Data[i].langTitle}</li>
+                            </ul>
+                        </div>
+                        <a href="${data[index].Data[i].langurl}" target="_blank" class="text-decoration-none"><button class="custom-card-link position-absolute rounded-circle p-2 border-0 d-flex align-items-center justify-content-center"><i class="fa fa-external-link-square-alt fs-4"></i></button></a>
                     </div>
-                </div>
                 </li>
         
                 `;
