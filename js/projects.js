64f806bfc7a4692d1f601397a1e2e6b34ee13969
kai <kailashjat604@gmail.com>
2023-06-08 16:00:33 +0530

    Refactor: restructure projects data

--- js/projects.js
+++ js/projects.js
@@ -22,37 +22,33 @@ fetch("../data/projects.json")
     $(function () {
       $("#type").change(function () {
         valu = $(this).val();
-        console.log(valu);
         let index = valu;
         $("#project").empty();
         $("#error").empty();
-        console.log(data[index].Data.length);
-        if (valu < data.length && data[index].Data.length > 0) {
-          for (let i = 0; i < data[index].Data.length; i++) {
+        if (valu < data.length) {
+          var html = "";
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].domainIds.includes(index))
             try {
-              console.log(data);
-              var html = "";
-              for (i = 0; i < data[index].Data.length; i++) {
                 html += `
                 <li class="cards_item">
                 <div class="card">
                     <div class="card_content">
-                    <p class="card_title">${data[index].Data[i].langName}</p>
-                    <span class="cardtitle">Project Admin-${data[index].Data[i].langAdmin}</span>
-                    <h6 class="card_title">Tech Stack- ${data[index].Data[i].langTitle}</h6>
-                    <p class="card_text">${data[index].Data[i].langDesc}</p>
-                    <button class="btn card_btn" onclick="window.location.href='${data[index].Data[i].langurl}'"target="blank">Repo Url</button>
+                    <p class="card_title">${data[i].langName}</p>
+                    <span class="cardtitle">Project Admin-${data[i].langAdmin}</span>
+                    <h6 class="card_title">Tech Stack- ${data[i].langTitle}</h6>
+                    <p class="card_text">${data[i].langDesc}</p>
+                    <button class="btn card_btn" onclick="window.location.href='${data[i].langurl}'"target="blank">Repo Url</button>
                     </div>
                 </div>
                 </li>
         
                 `;
-              }
-              $("#project").append(html);
             } catch (error) {
               console.log(error);
             }
           }
+          $("#project").append(html);
         } else {
           var htmlz = `<div class= "container unique-style3  mb-5 pb-5">`;
           htmlz += `<p class="text-light text-center text-no-data">No data found.Please select another month.</p>`;
