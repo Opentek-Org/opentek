f4b7f10d86ebf2c620e9d99ce757c5ab046c3a31
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-07-01 23:15:30 +0530

    Colortheme Issue Fixed (#389)
    * chat bot html code
    
    * chatbot CSS
    
    * successfully added chatbot v3.5
    
    * Prettier Check
    
    * fixed after login issue
    
    * Prettier checked
    
    * Added responsiveness to chatbot
    
    * Prettier Checked
    
    * Responsive 100%
    
    * Conflict solve
    
    * Fixed theme isssue

--- chatBot/script.js
+++ chatBot/script.js
@@ -7,7 +7,8 @@ const chatbotCloseBtn = document.querySelector('.chatbot__header span');
 let userMessage;
 // Need GPT key
 const inputInitHeight = chatInput.scrollHeight;
-const API_KEY = 'Enter API key here' //Important
+const API_KEY = 'Enter API key' //Important
+
 
 const createChatLi = (message, className) => {
   const chatLi = document.createElement('li');
@@ -24,7 +25,7 @@ const generateResponse = (incomingChatLi) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-RapidAPI-Key": "API_KEY", //enter key imp*
+      "X-RapidAPI-Key": "API_KEY ", //enter key imp*
       "X-RapidAPI-Host": "chatgpt53.p.rapidapi.com",
     },
     body: JSON.stringify({
