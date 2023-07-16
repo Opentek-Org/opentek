7caec31490ee546c0f3dbd646b53a7c8ef0a71a3
Ayush-Gupta <ayushgupta9951@gmail.com>
2023-07-16 10:41:07 -0700

    Add no projects available card (#493)

--- js/projects.js
+++ js/projects.js
@@ -19,32 +19,45 @@ $("#selectoption").append(htmlc);
 function loadData(data, valu) {
   let index = valu;
   if (valu == "all" || valu < domain.length) {
+    let isFound = false
     var html = "";
     for (let i = data.length -1; i >= 0 ; i--) {
-      if (valu == "all" || data[i].domainIds.includes(index))
-      try {
-          html += `
-          <li class="cards_item">
-          <div class="card">
-              <div class="card_content">
-              <p class="card_title">${data[i].langName}</p>
-              <div class="card-project-properties"> 
-              <span class="card-project-admin-name">Project Admin : ${data[i].langAdmin}</span>
-              <h6 class="card-stack"> ${data[i].langTitle}</h6>
-              </div>
-              <p class="card_text">${data[i].langDesc}</p>
-              </div>
-              <a href="${data[i].langurl}" target="_blank"><button class="btn card_btn">Github Repository</button></a>
-          </div>
-          </li>
-  
-          `;
-      } catch (error) {
-        console.log(error);
+      if (valu == "all" || data[i].domainIds.includes(index)) {
+        isFound = true
+        try {
+            html += `
+            <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+                <p class="card_title">${data[i].langName}</p>
+                <div class="card-project-properties"> 
+                <span class="card-project-admin-name">Project Admin : ${data[i].langAdmin}</span>
+                <h6 class="card-stack"> ${data[i].langTitle}</h6>
+                </div>
+                <p class="card_text">${data[i].langDesc}</p>
+                </div>
+                <a href="${data[i].langurl}" target="_blank"><button class="btn card_btn">Github Repository</button></a>
+            </div>
+            </li>
+    
+            `;
+        } catch (error) {
+          console.log(error);
+        }
       }
+    } 
+    if (!isFound) {
+      console.log("Not found")
+      html += `
+            <div class="not-found-card">
+                <div class="card_content not">No data found</div>
+            </div>
+            `;
     }
     $("#project").append(html);
   } else {
+    console.log(isFound)
+    console.log("error")
     var htmlz = `<div class= "container unique-style3  mb-5 pb-5">`;
     htmlz += `<p class="text-light text-center text-no-data">No data found.Please select another month.</p>`;
     htmlz += `</div>`;
