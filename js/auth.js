2c6f3904f833174a00d6f44c78882cafd49086fd
Akanksha Jha <100708686+Akanksha-202@users.noreply.github.com>
2023-07-20 13:53:29 +0530

    added firebase (#519)
    * added firebase
    
    * merge conflicts cleared

--- js/auth.js
+++ js/auth.js
@@ -1,68 +1,72 @@
-let auth0 = null;
+let firebaseConfig = null;
+
 const fetchAuthConfig = () => fetch("/auth_config.json");
 
 const configureClient = async () => {
   const response = await fetchAuthConfig();
-  const config = await response.json();
-
-  auth0 = await createAuth0Client({
-    domain: config.domain,
-    client_id: config.clientId,
-  });
+  firebaseConfig = await response.json();
+  firebase.initializeApp(firebaseConfig);
 };
 
 const updateUI = async () => {
-  const isAuthenticated = await auth0.isAuthenticated();
+  const user = firebase.auth().currentUser;
+  const isAuthenticated = !!user;
 
   document.getElementById("btn-logout").disabled = !isAuthenticated;
   document.getElementById("btn-login").disabled = isAuthenticated;
+  document.getElementById("btn-save").disabled = !isAuthenticated;
 
   document.getElementById("btn-logout").hidden = !isAuthenticated;
   document.getElementById("btn-login").hidden = isAuthenticated;
+  document.getElementById("btn-save").hidden = !isAuthenticated;
 
   if (isAuthenticated) {
-    // document.getElementById("gated-content-1").classList.remove("hidden");
-    // document.getElementById("gated-content-3").classList.remove("hidden");
-    // document.getElementById("gated-content-5").classList.remove("hidden");
-    // document.getElementById("gated-content-6").classList.remove("hidden");
     document.getElementById("gated-content-2").classList.add("hidden-p");
     document.getElementById("gated-content-4").classList.add("hidden-p");
 
-    // Features Event, Participate [REMOVE LISTENER]
     let tuneIn_A = document.getElementById("gated-content-1");
-    console.log("tuneIn_A");
-    tuneIn_A
-      .getElementsByTagName("a")[0]
-      .removeEventListener("click", login, true);
+    tuneIn_A.getElementsByTagName("a")[0].removeEventListener("click", login, true);
     let participate_A = document.getElementById("gated-content-3");
-    participate_A
-      .getElementsByTagName("a")[0]
-      .removeEventListener("click", login, true);
+    participate_A.getElementsByTagName("a")[0].removeEventListener("click", login, true);
 
-    // Footers Event, Participate [REMOVE LISTENER]
     let tuneIn_A_FOOTER = document.getElementById("gated-content-5");
     tuneIn_A_FOOTER.removeEventListener("click", login, true);
     let participate_A_FOOTER = document.getElementById("gated-content-6");
     participate_A_FOOTER.removeEventListener("click", login, true);
 
-    const claims = await auth0.getIdTokenClaims();
-    const pictureUrl = claims.picture;
+    const pictureUrl = user.photoURL;
     console.log(pictureUrl);
 
     document.getElementById("avatar-img").src = pictureUrl;
     document.getElementById("avatar-img-div").classList.remove("hidden-icon");
-  } else {
-    // Features Event, Programs [ADD LISTENER]
-    let tuneIn_A = document.getElementById("gated-content-1");
-    tuneIn_A
-      .getElementsByTagName("a")[0]
-      .addEventListener("click", login, true);
-    let participate_A = document.getElementById("gated-content-3");
-    participate_A
-      .getElementsByTagName("a")[0]
-      .addEventListener("click", login, true);
 
-    // Footer Event, Programs [ADD LISTENER]
+    const { uid } = user;
+
+    const firestore = firebase.firestore();
+    const userRef = firestore.collection("users").doc(uid);
+
+    const userDoc = await userRef.get();
+    if (!userDoc.exists) {
+      // User details don't exist in Firestore, create user data
+      const { displayName, email } = user;
+      const userDetails = {
+        uid,
+        displayName,
+        email,
+        saved: [],
+      };
+
+      await userRef.set(userDetails);
+      console.log("User details stored in Firestore");
+    } else {
+      console.log("User details already exist in Firestore");
+    }
+  } else {
+    let tuneIn_A = document.getElementById("gated-content-1");
+    tuneIn_A.getElementsByTagName("a")[0].addEventListener("click", login, true);
+    let participate_A = document.getElementById("gated-content-3");
+    participate_A.getElementsByTagName("a")[0].addEventListener("click", login, true);
+
     let tuneIn_A_FOOTER = document.getElementById("gated-content-5");
     tuneIn_A_FOOTER.addEventListener("click", login, true);
     let participate_A_FOOTER = document.getElementById("gated-content-6");
@@ -72,25 +76,24 @@ const updateUI = async () => {
 
 window.onload = async () => {
   await configureClient();
-  updateUI();
+  firebase.auth().onAuthStateChanged((user) => {
+    updateUI();
+  });
   const query = window.location.search;
   if (query.includes("code=") && query.includes("state=")) {
-    // Process the login state
-    await auth0.handleRedirectCallback();
+    await firebase.auth().getRedirectResult();
     updateUI();
-    // Use replaceState to redirect the user away and remove the querystring parameters
     window.history.replaceState({}, document.title, "/");
   }
 };
 
-const login = async () => {
-  await auth0.loginWithRedirect({
-    redirect_uri: window.location.origin,
-  });
+const login = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithRedirect(provider);
 };
 
 const logout = () => {
-  auth0.logout({
-    returnTo: window.location.origin,
+  firebase.auth().signOut().then(() => {
+    location.reload();
   });
 };
