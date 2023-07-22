7cbe736eae09270e9f1cfd27347bbb1a19c79b56
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-07-22 16:36:18 +0530

    Road Map dev Part 2

--- js/roadmap.js
+++ js/roadmap.js
@@ -1,5 +1,6 @@
-const data = {
-    frontendDev: "../images/roadmaps/frontenddev.jpg"
+const roadmapImage = {
+    frontendDev: "../images/roadmaps/frontenddev.jpg",
+    MERNStackDev: "../images/roadmaps/MERNStackDev.jpg"
 }
 
 function loadData(data) {
@@ -8,15 +9,15 @@ function loadData(data) {
     for (let i = 0; i < data.length; i++) {
        html +=  `
        <li class="cards_item">
-       <div class="card">
+       <div id ="${data[i].id}" class="raodmap-card">
            <div>
-           <img src= ""/>
+           <img src= "${roadmapImage[data[i].roadmap]}"/>
            </div>
            <div class="card_content">
            <p class="card_title">${data[i].title}</p>
            <p class="card_text">${data[i].description}</p>
            </div>
-          <button class="btn card_btn" onclick='getRoadMap("${data[i].roadmap}", "${data[i].title}")'>View Road Map</button>
+           <button class="btn btn-sm btn-outline-light" onclick='getRoadMap("${data[i].roadmap}", "${data[i].title}")'>View Road Map</button>
        </div>
        </li>
    `     
@@ -37,7 +38,7 @@ const roadmap = $("#roadmap");
 function getRoadMap(name, head){
     roadmap.css("display", "block");
     $("body").css("height", "100vh");
-    $("#roadimg").html(`<div><h1>${head} <br/>RoadMap </h1><img src="${data[name]}" alt="roadmap_img"/></div>`)
+    $("#roadimg").html(`<div><h1>${head} <br/>RoadMap </h1><img src="${roadmapImage[name]}" alt="roadmap_img"/></div>`)
 }
 
 function closeRoadMap(){
