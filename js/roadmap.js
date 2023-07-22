51463e4ac2cc4fd63fa32299665aa21414050866
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-07-22 11:34:47 +0530

    RoadMap Dev Part 1

---
+++ js/roadmap.js
@@ -0,0 +1,47 @@
+const data = {
+    frontendDev: "../images/roadmaps/frontenddev.jpg"
+}
+
+function loadData(data) {
+    let html =  "";
+    
+    for (let i = 0; i < data.length; i++) {
+       html +=  `
+       <li class="cards_item">
+       <div class="card">
+           <div>
+           <img src= ""/>
+           </div>
+           <div class="card_content">
+           <p class="card_title">${data[i].title}</p>
+           <p class="card_text">${data[i].description}</p>
+           </div>
+          <button class="btn card_btn" onclick='getRoadMap("${data[i].roadmap}", "${data[i].title}")'>View Road Map</button>
+       </div>
+       </li>
+   `     
+    }
+   
+   $("#roadmapslist").html(html);
+  
+  }
+  
+  fetch("../data/roadmaps.json")
+    .then((data) => data.json())
+    .then((data) => {
+     loadData(data)
+    });
+
+const roadmap = $("#roadmap");
+
+function getRoadMap(name, head){
+    roadmap.css("display", "block");
+    $("body").css("height", "100vh");
+    $("#roadimg").html(`<div><h1>${head} <br/>RoadMap </h1><img src="${data[name]}" alt="roadmap_img"/></div>`)
+}
+
+function closeRoadMap(){
+    roadmap.css("display", "none");
+    $("body").css("height", "auto")
+}
+  
