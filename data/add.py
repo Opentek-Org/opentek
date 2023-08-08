d5d7f243b564b91f9125753a04e125c677618eb7
Simran <simran28singh28@gmail.com>
2023-08-08 23:57:08 +0530

    Enhance Firestore Data Insertion Script

--- data/add.py
+++ data/add.py
@@ -4,19 +4,29 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate(r"C:\Users\ANIRUDH PANDA\Downloads\opentek-serviceKey.json")
+cred = credentials.Certificate("path/to/your/serviceKey.json")
 firebase_admin.initialize_app(cred)
 
 # Create a Firestore client
 db = firestore.client()
 
 # Read data from projects.json file
-with open(r"C:\Users\ANIRUDH PANDA\OneDrive\Desktop\opentek\data\projects.json", encoding='utf-8') as json_file:
-    data = json.load(json_file)
+try:
+    with open("path/to/your/projects.json", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+except FileNotFoundError:
+    print("Error: JSON file not found.")
+    exit(1)
+except json.JSONDecodeError:
+    print("Error: JSON file is not valid.")
+    exit(1)
 
 # Add data to Firestore collection
 for item in data:
-    doc_ref = db.collection('projects').document()
-    doc_ref.set(item)
+    try:
+        doc_ref = db.collection("projects").add(item)
+    except Exception as e:
+        print("Error adding data to Firestore:", str(e))
 
 print("Data added to Firestore successfully!")
+
